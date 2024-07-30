15ed445f23d4d266da917dac1515532b23923fe6
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-07-30 11:04:26 +0530

    Update JS-30-Jul.js

--- Practice - Day 2__30-July-24/JS-30-Jul.js
+++ Practice - Day 2__30-July-24/JS-30-Jul.js
@@ -16,22 +16,22 @@
 
 // Q8. Write a program to calculate compound interest
 
-    // let principal = 6250
+    let principal = 6250
 
-    // let rateOfIntrest = 8
+    let rateOfIntrest = 8
 
-    // let numberOfYears = 2
+    let numberOfYears = 2
 
-    // if (principal !== 0 && rateOfIntrest !== 0 && numberOfYears !== 0) {
+    if (principal !== 0 && rateOfIntrest !== 0 && numberOfYears !== 0) {
         
-    //     let ammount = principal * Math.pow((1 + rateOfIntrest / 100),numberOfYears) 
-    //     console.log("The Total Ammount is = " + ammount);
-    //     if (ammount !== 0) {
+        let ammount = principal * Math.pow((1 + rateOfIntrest / 100),numberOfYears) 
+        console.log("The Total Ammount is = " + ammount);
+        if (ammount !== 0) {
             
-    //         let compoundIntrest = ammount - principal
-    //         console.log("The Total Compound Intrest is = " + compoundIntrest);
-    //     } 
-    // }
+            let compoundIntrest = ammount - principal
+            console.log("The Total Compound Intrest is = " + compoundIntrest);
+        } 
+    }
 
 
 //*********************************************************************************************
@@ -41,17 +41,17 @@
 
 // Q9. Finding maximun of two number
 
-    // let x = 5
+    let x = 5
 
-    // let y = 10
+    let y = 10
 
-    // if (x>y) {
+    if (x>y) {
         
-    //     console.log("x Is Greater than y = " + x);
+        console.log("x Is Greater than y = " + x);
 
-    // } else {
-    //     console.log("y Is Greater Than x = " + y);
-    // }
+    } else {
+        console.log("y Is Greater Than x = " + y);
+    }
 
 
 //*********************************************************************************************
@@ -61,14 +61,14 @@
 
 // Q10. Number is positive or negative
 
-    // let x = -10
+    let x = -10
 
-    // if (x > 0 && x !== 0) {
+    if (x > 0 && x !== 0) {
         
-    //     console.log("This Number is Positive = " + x);
-    // } else {
-    //     console.log("This Number is Negative = " + x);
-    // }
+        console.log("This Number is Positive = " + x);
+    } else {
+        console.log("This Number is Negative = " + x);
+    }
 
 
 //*********************************************************************************************
@@ -79,15 +79,15 @@
 // Q11. Number is even or odd
 
 
-    // let x = 7
+    let x = 7
 
-    // if (x % 2 == 0) {
+    if (x % 2 == 0) {
         
-    //     console.log("This is an Even Number = " + x);
+        console.log("This is an Even Number = " + x);
 
-    // } else {
-    //     console.log("This is an Odd Number = " + x);
-    // }
+    } else {
+        console.log("This is an Odd Number = " + x);
+    }
 
 
 //*********************************************************************************************
@@ -96,17 +96,17 @@
 
 // Q12. check age of a person
 
-    // let x = 1999 
+    let x = 1999 
 
-    // let y = 2024
+    let y = 2024
 
-    // if (x !== 0 && y !== 0) {
+    if (x !== 0 && y !== 0) {
         
-    //     let age = y-x
-    //     console.log(age);
-    // } else {
-    //     console.log("Put The Right Birth Year In Valiable x");
-    // }
+        let age = y-x
+        console.log(age);
+    } else {
+        console.log("Put The Right Birth Year In Valiable x");
+    }
 
 
 //*********************************************************************************************
@@ -117,15 +117,15 @@
 // Q13. check if a person is eligible for offer
 
 
-    // let x = 17
+    let x = 17
 
-    // if (x > 18) {
+    if (x > 18) {
         
-    //     console.log("You are eligible for offer " + x);
+        console.log("You are eligible for offer " + x);
 
-    // } else {
-    //     console.log("You are not eligible for offer yet sorry " + x);
-    // }
+    } else {
+        console.log("You are not eligible for offer yet sorry " + x);
+    }
 
 
 //*********************************************************************************************
@@ -134,20 +134,20 @@
 
 // Q14. Finding maximum of 3 number
 
-    // let x = 5
+    let x = 5
 
-    // let y = 15
+    let y = 15
 
-    // let z = 10
+    let z = 10
 
 
-    // if (x > y && x > z) {
-    //     console.log("x is the grater number of all of them " + x);
-    // } else if (y > x && y > z) {
-    //     console.log("y is the greater number of all of them " + y);
-    // } else {
-    //     console.log("z is the greater number of all of them " + z);
-    // }
+    if (x > y && x > z) {
+        console.log("x is the grater number of all of them " + x);
+    } else if (y > x && y > z) {
+        console.log("y is the greater number of all of them " + y);
+    } else {
+        console.log("z is the greater number of all of them " + z);
+    }
 
 //*********************************************************************************************
 //*********************************************************************************************
@@ -157,24 +157,24 @@
 // Q15. Display grade for students marks
 
 
-    // let marks = 17
+    let marks = 17
 
-    // if (marks > 90) {
-    //     console.log("A+ Grade Marks Obtained");
-    // } else if (marks > 80) {
-    //     console.log("A Grade Marks Obtained");
-    // } else if (marks > 70) {
-    //     console.log("B+ Grade Marks Obtained");
-    // } else if (marks > 60) {
-    //     console.log("B Grade Marks Obtained");
-    // } else if (marks > 50) {
-    //     console.log("C+ Grade Marks Obtained");
-    // } else if (marks > 40) {
-    //     console.log("C Grade Marks Obtained");
-    // } else if (marks > 30) {
-    //     console.log("D Grade Marks Obtained");
-    // } else if (marks > 20) {
-    //     console.log("E Grade Marks Obtained");
-    // } else {
-    //     console.log("You are Fail Go School Again");
-    // }
+    if (marks > 90) {
+        console.log("A+ Grade Marks Obtained");
+    } else if (marks > 80) {
+        console.log("A Grade Marks Obtained");
+    } else if (marks > 70) {
+        console.log("B+ Grade Marks Obtained");
+    } else if (marks > 60) {
+        console.log("B Grade Marks Obtained");
+    } else if (marks > 50) {
+        console.log("C+ Grade Marks Obtained");
+    } else if (marks > 40) {
+        console.log("C Grade Marks Obtained");
+    } else if (marks > 30) {
+        console.log("D Grade Marks Obtained");
+    } else if (marks > 20) {
+        console.log("E Grade Marks Obtained");
+    } else {
+        console.log("You are Fail Go School Again");
+    }
